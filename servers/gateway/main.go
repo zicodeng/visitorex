c6eb81a28febbb6ad3a87571171aa3285ba87436
Zico Deng <zicodeng@gmail.com>
2017-12-12 17:51:38 -0800

    Initialize Redis connection in main.go

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/go-redis/redis"
+	"github.com/zicodeng/visitorex/servers/gateway/sessions"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 // main is the main entry point for the server.
@@ -17,12 +20,35 @@ func main() {
 		log.Fatal("Please set TLS_CERT and TLS_KEY environment variables")
 	}
 
+	// sessionKey is the signing key for session token.
+	sessionKey := os.Getenv("SESSION_KEY")
+	if len(sessionKey) == 0 {
+		log.Fatal("Please set SESSION_KEY environment variable")
+	}
+
 	// Address the server should listen on.
 	// If empty, default to ":443".
 	serverAddr := os.Getenv("SERVER_ADDR")
 	if len(serverAddr) == 0 {
 		serverAddr = ":443"
 	}
+
+	// Set up Redis connection.
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if len(redisAddr) == 0 {
+		redisAddr = ":6379"
+	}
+
+	// Shared Redis client.
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+
+	// Redis store for session state.
+	sessionStore := sessions.NewRedisStore(redisClient, time.Hour)
+
+	// Initialize HandlerContext.
+	ctx := handlers.NewHandlerContext(sessionKey, sessionStore)
 
 	mux := http.NewServeMux()
 
