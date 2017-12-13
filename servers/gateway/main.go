bc821041aece13e942f562f5386f8a5d38a8afe4
Zico Deng <zicodeng@gmail.com>
2017-12-12 17:40:09 -0800

    Refactor code

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -9,19 +9,19 @@ import (
 // main is the main entry point for the server.
 func main() {
 
-	// Address the server should listen on.
-	// If empty, default to ":443".
-	serverAddr := os.Getenv("SERVER_ADDR")
-	if len(serverAddr) == 0 {
-		serverAddr = ":443"
-	}
-
 	// Path to TLS public certificate.
 	TLSCert := os.Getenv("TLS_CERT")
 	// Path to the associated private key.
 	TLSKey := os.Getenv("TLS_KEY")
 	if len(TLSCert) == 0 || len(TLSKey) == 0 {
 		log.Fatal("Please set TLS_CERT and TLS_KEY environment variables")
+	}
+
+	// Address the server should listen on.
+	// If empty, default to ":443".
+	serverAddr := os.Getenv("SERVER_ADDR")
+	if len(serverAddr) == 0 {
+		serverAddr = ":443"
 	}
 
 	mux := http.NewServeMux()
