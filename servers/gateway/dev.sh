51ea8ce7936aa0c277d4d7243f416c47bbae8126
Zico Deng <zicodeng@gmail.com>
2017-12-12 17:56:24 -0800

    Add Redis starting container script to dev script

--- servers/gateway/dev.sh
+++ servers/gateway/dev.sh
@@ -6,5 +6,21 @@ export SERVER_ADDR=localhost:443
 
 export TLS_CERT="$(pwd)/tls/fullchain.pem"
 export TLS_KEY="$(pwd)/tls/privkey.pem"
+export SESSION_KEY="hello world"
+
+export REDIS_ADDR=192.168.99.100:6379
+
+export REDIS_CONTAINER=redis-server
+
+if [ "$(docker ps -aq --filter name=$REDIS_CONTAINER)" ]; then
+    docker rm -f $REDIS_CONTAINER
+fi
+
+# Run Redis Docker container.
+docker run \
+-d \
+--name $REDIS_CONTAINER \
+-p 6379:6379 \
+redis
 
 go run main.go
