4abd9f1370871b6fe9262b917435fb03ca804f51
Zico Deng <zicodeng@gmail.com>
2017-12-26 16:47:22 -0800

    Remove unused import

--- client/src/components/admin/index.tsx
+++ client/src/components/admin/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import * as adminActions from 'components/admin/actions';
-import * as materialFormActions from 'components/material-form/actions';
 import MaterialForm, {
     FORM_TYPES,
     Input,
