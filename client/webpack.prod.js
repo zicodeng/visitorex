5488339167927eb2f575c7ac7ca157f777f5caf5
Zico Deng <zicodeng@gmail.com>
2018-01-14 22:56:43 -0800

    Upate webpack config file comment

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -6,14 +6,15 @@ var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(common, {
     devtool: 'source-map',
     plugins: [
+        // Inject production as NODE_ENV in webpack build process.
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
+                NODE_ENV: JSON.stringify('production'),
+            },
         }),
         // Webpack built-in UglifyJsPlugin doesn't work with webpack-dev-server version 2.8.0+
         new UglifyJsPlugin({
-            sourceMap: true
-        })
-    ]
+            sourceMap: true,
+        }),
+    ],
 });
