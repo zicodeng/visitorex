32d4b3ce041fc53e3af010bc1f2ae07440bdc092
Zico Deng <zicodeng@gmail.com>
2018-01-14 23:12:13 -0800

    Disable logger in production environment

--- client/src/store.ts
+++ client/src/store.ts
@@ -7,7 +7,11 @@ import rootReducer from 'root-reducer';
 
 // Middleware order matters!
 // thunk needs to be placed before logger in order to avoid undefined action error.
-const middleware = applyMiddleware(thunk, logger, promiseMiddleware());
-const store = createStore(rootReducer, middleware);
+let middleware = [thunk, promiseMiddleware()];
+if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
