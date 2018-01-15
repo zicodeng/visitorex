de98d4996c89744f465882fccdb971771b10f581
Zico Deng <zicodeng@gmail.com>
2018-01-14 23:38:55 -0800

    Set locale to default

--- servers/visitor/handlers/office.js
+++ servers/visitor/handlers/office.js
@@ -173,7 +173,7 @@ const OfficeHandler = (officeStore, visitorStore, visitorTrie) => {
         }
 
         // Automatically assigns current date when a new visitor checks in.
-        moment.locale('en-US');
+        moment.locale();
         const visitDate = moment().format('l');
         const visitTime = moment().format('LT');
 
